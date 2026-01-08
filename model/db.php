7580c6b969f8dd796043e2d35956575146b6e4c0
Shahriyar Hasan <85627941+ShahriyarH10@users.noreply.github.com>
2026-01-08 07:04:13 +0600

    Implement Database Connection

--- model/db.php
+++ model/db.php
@@ -3,9 +3,7 @@ $host = "127.0.0.1";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "tournament_db";
-
-function getConnection()
-{
+function getConnection(){
     global $host, $dbuser, $dbpass, $dbname;
     $con = mysqli_connect($host, $dbuser, $dbpass, $dbname);
     return $con;
